92788c6f14505aeed18fe96e20ab165247c9ffcf
Itunu <hitunes4@gmail.com>
2025-05-27 00:05:24 +0100

    initial commit tests folder

--- tests/testthat/test-prepare_data.R
+++ tests/testthat/test-prepare_data.R
@@ -1,8 +1,8 @@
 test_that("returns a data.frame with the correct columns and types", {
-  wig <- system.file("extdata", "DNaseI_BG3_gr_chr4.bedGraph.gz", package = "HMRFHiC")
-  chain <- system.file("extdata", "dm3ToDm6.over.chain", package = "HMRFHiC")
-  te <- system.file("extdata", "dm6_TEs_chr4.gtf.gz", package = "HMRFHiC")
-  hic <- system.file("extdata", "BG3_WT_merged_hic_chr4_2Kb.cool", package = "HMRFHiC")
+  wig <- system.file("extdata", "DNaseI_BG3_gr_chr4.bedGraph", package = "HMRFHiC")
+  chain <- system.file("extdata", "dm3ToDm6_chr4_only.chain", package = "HMRFHiC")
+  te <- system.file("extdata", "dm6_TEs_chr4.gtf", package = "HMRFHiC")
+  hic <- system.file("extdata", "BG3_WT_merged_hic_matrix_chr4_100Kb.cool", package = "HMRFHiC")
 
   expect_true(file.exists(wig))
   expect_true(file.exists(chain))
@@ -13,8 +13,8 @@ test_that("returns a data.frame with the correct columns and types", {
     file_path      = hic,
     chr            = "chr4",
     start          = 1,
-    end            = 10000,
-    resolution     = 2000,
+    end            = 100000,
+    resolution     = 10000,
     genome_package = "BSgenome.Dmelanogaster.UCSC.dm6",
     acc_wig        = wig,
     chain_file     = chain,
@@ -28,33 +28,33 @@ test_that("returns a data.frame with the correct columns and types", {
   expect_type(bb$interactions, "double")
 })
 
-test_that("builds full 2kb × 2kb grid over 1–40kb", {
-  hic <- system.file("extdata", "BG3_WT_merged_hic_chr4_2Kb.cool", package = "HMRFHiC")
+test_that("builds full 20kb × 20kb grid over 1–40kb", {
+  hic <- system.file("extdata", "BG3_WT_merged_hic_matrix_chr4_100Kb.cool", package = "HMRFHiC")
   bb <- get_data(
     file_path      = hic,
     chr            = "chr4",
     start          = 1,
-    end            = 10000,
-    resolution     = 2000,
+    end            = 100000,
+    resolution     = 20000,
     genome_package = "BSgenome.Dmelanogaster.UCSC.dm6"
   )
   expect_equal(nrow(bb), 5 * 5)
   expect_equal(bb$start[1], 1L)
-  expect_equal(bb$end.i.[1], 2000L)
+  expect_equal(bb$end.i.[1], 20000L)
 })
 
 test_that("GC, ACC, and TES are in valid ranges or NA", {
-  wig <- system.file("extdata", "DNaseI_BG3_gr_chr4.bedGraph.gz", package = "HMRFHiC")
-  chain <- system.file("extdata", "dm3ToDm6.over.chain", package = "HMRFHiC")
-  te <- system.file("extdata", "dm6_TEs_chr4.gtf.gz", package = "HMRFHiC")
-  hic <- system.file("extdata", "BG3_WT_merged_hic_chr4_2Kb.cool", package = "HMRFHiC")
+  wig <- system.file("extdata", "DNaseI_BG3_gr_chr4.bedGraph", package = "HMRFHiC")
+  chain <- system.file("extdata", "dm3ToDm6_chr4_only.chain", package = "HMRFHiC")
+  te <- system.file("extdata", "dm6_TEs_chr4.gtf", package = "HMRFHiC")
+  hic <- system.file("extdata", "BG3_WT_merged_hic_matrix_chr4_100Kb.cool", package = "HMRFHiC")
 
   bb <- get_data(
     file_path      = hic,
     chr            = "chr4",
     start          = 1,
-    end            = 10000,
-    resolution     = 2000,
+    end            = 100000,
+    resolution     = 10000,
     genome_package = "BSgenome.Dmelanogaster.UCSC.dm6",
     acc_wig        = wig,
     chain_file     = chain,
